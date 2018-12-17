163743fb32544085e9dea60d62e31979e445f652
fengshuan <1263215592@qq.com>
2018-12-17 23:12:40 +0800

    feat:selenium-webdriver

---
+++ test/webdriver.js
@@ -0,0 +1,39 @@
+const { expect } = require('chai');
+const webdriver = require('selenium-webdriver');
+const By = webdriver.By;
+const chromeDriver = require('selenium-webdriver/chrome');
+
+describe('百度首页UI测试', function() {
+    this.timeout(50000);
+
+    let driver;
+
+    before(() => {
+        driver = new webdriver.Builder()
+        .forBrowser('chrome')
+        .setChromeOptions(new chromeDriver.Options().addArguments(['headless']))
+        .build();
+    });
+
+    it('should have title "百度一下，你就知道"', done => {
+        driver.get('https://www.baidu.com').then(() => {
+            driver.getTitle().then(title => {
+                expect(title).to.equal('百度一下，你就知道');
+                done();
+            });
+        });
+    });
+
+    it('should have button value "百度一下', done => {
+        driver.findElement(By.id('su')).then(button => {
+            button.getAttribute('value').then(val => {
+                expect(val).to.equal('百度一下');
+                done()
+            })
+        })
+    })
+
+    after(() => {
+        driver.quit();
+    })
+})
